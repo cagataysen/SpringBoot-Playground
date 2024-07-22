62ac95ddb7b7238e4d25b039a2031227b33847ea
Cagatay Sen <orhancagataysen@outlook.com>
2024-07-22 14:08:13 +0200

    dopdisc created and configured, connected with db

---
+++ dopdisc/src/test/java/com/dopdisc/demo/DopdiscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dopdisc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopdiscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
