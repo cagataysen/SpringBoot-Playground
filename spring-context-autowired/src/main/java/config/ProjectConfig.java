2019dee0e2794006ee239ee9bb9a943d52e5957f
Cagatay Sen <orhancagataysen@outlook.com>
2025-04-24 14:58:58 +0200

    context - service - repo - null object diff

---
+++ spring-context-autowired/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = {"services", "repositories"})
+public class ProjectConfig {
+
+
+}
