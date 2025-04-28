99187a922366bf574618dc65e4926df8b8cb9ba0
Cagatay Sen <orhancagataysen@outlook.com>
2025-04-28 11:27:09 +0200

    bean names used

--- spring-context/src/main/java/config/ProjectConfig.java
+++ spring-context/src/main/java/config/ProjectConfig.java
@@ -3,14 +3,24 @@ package config;
 import beans.MyBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
-    @Bean
-    public MyBean myBean() {
+    @Bean("A")
+    public MyBean myBean1() {
         MyBean b = new MyBean();
         b.setText("Hello");
+        return b;
+    }
+
+    //HAS NO NAME, SO METHOD NAME IS THE NAME OF IT.
+    @Bean
+    @Primary
+    public MyBean myBean2() {
+        MyBean b = new MyBean();
+        b.setText("World");
         return b;
     }
 
--- spring-context/src/main/java/main/Main.java
+++ spring-context/src/main/java/main/Main.java
@@ -14,9 +14,18 @@ public class Main {
     public static void main(String[] args) {
         try( var context =
                      new AnnotationConfigApplicationContext(ProjectConfig.class)) {
+
+            //by type
+            /*
             MyBean b1 = context.getBean(MyBean.class);
             MyBean b2 = context.getBean(MyBean.class);
             MyBean b3 = context.getBean(MyBean.class);
+            */
+
+            //by name
+            MyBean b1 = context.getBean("A", MyBean.class);
+            MyBean b2 = context.getBean("myBean2", MyBean.class);
+            MyBean b3 = context.getBean("A", MyBean.class);
 
             // due to default - singleton it will be same bean
             System.out.println(b1.getText());
